e1845f0864ea1411cab440e2d75898dfb50f0530
kzerg <xinghe3@163.com>
2018-04-24 18:22:24 +0800

    add automapper by dll

---
+++ DC.ETL.Domain/Service/ExtractUnitStrategyService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DC.ETL.Domain.Specifications;
+using System.Linq.Expressions;
+using DC.ETL.Domain.Model;
+
+namespace DC.ETL.Domain.Service
+{
+    public class ExtractUnitStrategyService
+    {
+        /// <summary>
+        /// 保存选取的策略
+        /// </summary>
+        /// <returns>Schema模式集合</returns>
+        public int SaveStrategy(Guid SNextractUnit, ICollection<Guid> SNStrategies)
+        {
+            //if (SNextractUnit == null || SNStrategies == null) return -1;// TODO: 替换标准错误代码
+            //ExtractUnit extractUnit = iExtractUnitRepository.GetByKey(SNextractUnit);
+
+            //Expression<Func<Strategy, bool>> ex = t => SNStrategies.Contains(t.SN);
+            //IEnumerable<Strategy> Strategies = iStrategyRepository.GetAll(new ExpressionSpecification<Strategy>(ex));
+            //extractUnit.Strategies = Strategies.ToArray();
+            //iExtractUnitRepository.Update(extractUnit);
+            //return iExtractUnitRepository.SaveChanges();
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DC.ETL.Infrastructure/Utils/AutoMapperUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoMapper;
+
+namespace DC.ETL.Infrastructure.Utils
+{
+    public class AutoMapperUtils
+    {
+        /// <summary>
+        /// 注册映射关系
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TDto"></typeparam>
+        public static void Register<T, TDto>()
+        {
+            Mapper.Initialize(cfg => cfg.CreateMap<T, TDto>());
+        }
+        /// <summary>
+        /// 一般转换对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TDto"></typeparam>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public TDto Map<T, TDto>(T t)
+        {
+            return Mapper.Map<TDto>(t);
+        }
+    }
+}
